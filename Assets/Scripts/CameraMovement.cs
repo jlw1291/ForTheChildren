4f86ba2e1b52ef3967b42cc9a40b7b4ca9d6c852
jlw1291 <56693768+jlw1291@users.noreply.github.com>
2021-03-20 13:30:15 -0500

    Overworld Creation | Added 3 Blank Rooms
    Created 3 extra blank rooms.
    
    Each room needs distinct appearance.
    Forest
    Pond
    Town

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform target;
-    public float smoothing;
-    public Vector2 maxPosition;
-    public Vector2 minPosition;
+    public Transform target; //Target of the camera
+    public float smoothing; //Smooths camera adjustment
+    public Vector2 maxPosition; //x,y vector for setting maximum boundary position
+    public Vector2 minPosition; //x,y vector for setting minimum boundary position
 
 
     // Start is called before the first frame update
@@ -19,11 +19,13 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if(transform.position != target.position)
+        if(transform.position != target.position) //If not the same position as target player, begin adjustments
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //Establishes the camera boundary
             targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
             targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+            //Sets the follow for the camera as the player/target moves
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
     }
